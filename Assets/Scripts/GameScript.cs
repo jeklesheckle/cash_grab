e2d2daad6f4b3bd692d0d8de98e770c952cde393
Jek Desktop Win10 <jackpielstick@gmail.com>
2019-09-18 22:18:16 -0400

    multiple, see description
    Made UI a little closer to useable by setting game resolution to 2560x1440
    Made buttons currently on UI scale with screen size
    Made all buttons snap to center / corner
    Added new ui background, probably going to replace with actual ui objects later
    Made a coin prefab spawn every few seconds. need to add location randomization and stuff.

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -5,8 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class GameScript : MonoBehaviour
 {
+    public GameObject coinObject;
+    public int minCoinSpawnDelay, maxCoinSpawnDelay;
+
+    private float timeSinceLastCoin;
+    private float coinDelay;
+
+    private void Start()
+    {
+        timeSinceLastCoin = 0;
+        coinDelay = setCoinDelay();
+    }
+
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void OpenShop()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    private void Update()
+    {
+        if (timeSinceLastCoin  > coinDelay)
+        {
+            coinDelay = setCoinDelay();
+            SpawnCoin();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timeSinceLastCoin += Time.deltaTime;
+    }
+
+    private float setCoinDelay()
+    {
+        return Random.Range(minCoinSpawnDelay, maxCoinSpawnDelay);
+    }
+    private void SpawnCoin()
+    {
+        timeSinceLastCoin = 0;
+        GameObject coin = Instantiate (coinObject, new Vector3(0, 0, 0), coinObject.transform.rotation);
+        coin.transform.localScale = new Vector3(0.25f, 0.25f, 1);
     }
 }
