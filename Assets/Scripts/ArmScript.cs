b153d70b4554e2bcb6267685f08573e8345b181b
Jek Desktop Win10 <jackpielstick@gmail.com>
2019-09-29 02:18:36 -0400

    create arms
    UNFINISHED.
    Added a basic arm script and CreateArm() method for GameScript.
    Need to make the two work together so that arms actually move and face coins.

---
+++ Assets/Scripts/ArmScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour
+{
+    public Vector2 target;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 offset = new Vector3(target.x, target.y, 0) - transform.position;
+
+        // Construct a rotation as in the y+ case.
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, offset);
+
+        // Apply a compensating rotation that twists x+ to y+ before the rotation above.
+        transform.rotation = rotation * Quaternion.Euler(0, 0, -90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime) * moveSpeed;
+    }
+}
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -6,14 +6,17 @@ using UnityEngine.SceneManagement;
 public class GameScript : MonoBehaviour
 {
     public GameObject coinObject;
+    public GameObject armObject;
     public int minCoinSpawnDelay, maxCoinSpawnDelay;
     public Vector2 bottomLeftOfCoinSpawn, topRightOfCoinSpawn;
+    public Vector2 bottomLeftOfScreen, topRightOfScreen;
 
     private float timeSinceLastCoin;
     private float coinDelay;
 
-    private float coinXCoord;
-    private float coinYCoord;
+    private float coinXCoord, coinYCoord;
+    private float armXCoord, armYCoord;
+    private int armSide;
 
     private void Start()
     {
@@ -57,5 +60,32 @@ public class GameScript : MonoBehaviour
 
         GameObject coin = Instantiate (coinObject, new Vector3(coinXCoord, coinYCoord, 0), coinObject.transform.rotation);
         coin.transform.localScale = new Vector3(0.25f, 0.25f, 1);
+        SpawnArm();
+    }
+
+    private void SpawnArm()
+    {
+        armSide = Random.Range(1,5);
+        switch (armSide)
+        {
+            case 1: //top edge
+                armXCoord = Random.Range(bottomLeftOfScreen.x, topRightOfScreen.x);
+                armYCoord = topRightOfScreen.y;
+                break;
+            case 2: //right edge
+                armXCoord = topRightOfScreen.x;
+                armYCoord = Random.Range(bottomLeftOfScreen.y, topRightOfScreen.y);
+                break;
+            case 3: //bottom edge
+                armXCoord = Random.Range(bottomLeftOfScreen.x, topRightOfScreen.x);
+                armYCoord = bottomLeftOfScreen.y;
+                break;
+            case 4: //left edge
+                armXCoord = bottomLeftOfScreen.x;
+                armYCoord = Random.Range(bottomLeftOfScreen.y, topRightOfScreen.y);
+                break;
+        }
+        
+        GameObject arm = Instantiate(armObject, new Vector3(armXCoord, armYCoord, 0), armObject.transform.rotation);
     }
 }
